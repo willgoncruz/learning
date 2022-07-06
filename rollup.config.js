ff017fbf87b078255ca516638f22099c7897b7e3
William Cruz <william.cruz@quintoandar.com.br>
2022-07-06 15:13:29 -0300

    start: Rollup build and dev env

---
+++ rollup.config.js
@@ -0,0 +1,59 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import external from 'rollup-plugin-peer-deps-external';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import replace from '@rollup/plugin-replace';
+
+
+export default {
+  input: 'src/index.tsx',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('development')
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      serve(),
+      livereload(),
+  ],
+};
+
+
+// import { terser } from 'rollup-plugin-terser';
+// import postcss from 'rollup-plugin-postcss';
+
+// const packageJson = require('./package.json');
+
+// export default {
+//     input: 'src/index.ts',
+//     output: [
+//         {
+//             file: packageJson.main,
+//             format: 'cjs',
+//             sourcemap: true,
+//             name: 'react-lib'
+//         },
+//         {
+//             file: packageJson.module,
+//             format: 'esm',
+//             sourcemap: true
+//         }
+//     ],
+//     plugins: [
+//         external(),
+//         resolve(),
+//         commonjs(),
+//         typescript({ tsconfig: './tsconfig.json' }),
+//         postcss(),
+//         terser()
+//     ]
+// }
---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+/**
+ * Organização macro do projeto:
+ *
+ * Pages: São a estrutura que encapsula cada página de rota possível na aplicação
+ * Model: Contém os tipos e modelos utilizados
+ * API: Possui a integração com a chamada de requisições para as APIs disponíveis
+ * Components: Composto pelos componentes que compõe as páginas ou outros componentes que fazem sentido existirem de forma independente
+ */
+
+/**
+ * Nessa secção, comentarei as principais escolhas do projeto.
+ *
+ * React: foi escolhido pela familiariade com a lib
+ * Axios: pela simplicidade de realizar requisições e tratamento das respostas
+ * React Router: pela simplicidade de criação de novas telas
+ * Cypress: framework versátil para testes de tela, já utilizado anteriormente
+ * React Testing Lib: pela naturalidade da integração com React
+ * Styled Components: Modulariação e encapsulamento do css, excelente para criação rápida e concisa de estilos
+ */
+
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<div>TESTE</div>);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
